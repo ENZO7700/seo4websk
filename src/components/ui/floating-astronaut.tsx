2c8af2b6f4bb12889c5e4b501fdd3d788aaa6aa7
ENZO F <enzoenzof2024@gmail.com>
2025-07-31 03:25:29 +0000

    teraz tto musi byt ale na 100)0%

--- src/components/ui/floating-astronaut.tsx
+++ src/components/ui/floating-astronaut.tsx
@@ -8,25 +8,25 @@ import { motion } from 'framer-motion';
 const FloatingAstronaut = () => {
     return (
         <motion.div
-            className="absolute top-1/4 left-1/4 w-32 h-32 md:w-48 md:h-48 z-0"
+            className="absolute top-1/4 left-1/4 w-32 h-32 md:w-48 md:h-48 z-0 opacity-70"
             initial={{ y: -20, opacity: 0 }}
             animate={{
-                y: 0,
-                opacity: 1,
+                y: [0, 20, 0],
                 rotate: [0, 5, -5, 0],
+                opacity: 0.7
             }}
             transition={{
                 delay: 0.5,
                 duration: 2,
                 ease: "easeInOut",
                 y: {
-                    duration: 4,
+                    duration: 8,
                     repeat: Infinity,
                     repeatType: "mirror",
                     ease: "easeInOut",
                 },
                 rotate: {
-                    duration: 10,
+                    duration: 12,
                     repeat: Infinity,
                     ease: "easeInOut",
                 }
