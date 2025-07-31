a68daeebc5ab5c696822c80d2a15663a1f86e8bd
ENZO F <enzoenzof2024@gmail.com>
2025-07-31 03:23:21 +0000

    nic nevidim ioprav to sprav hlbkovu analyzu preeco to nefunguje

--- src/components/ui/floating-astronaut.tsx
+++ src/components/ui/floating-astronaut.tsx
@@ -9,15 +9,27 @@ const FloatingAstronaut = () => {
     return (
         <motion.div
             className="absolute top-1/4 left-1/4 w-32 h-32 md:w-48 md:h-48 z-0"
+            initial={{ y: -20, opacity: 0 }}
             animate={{
-                y: ["0%", "5%", "0%"],
-                rotate: [0, 5, 0],
+                y: 0,
+                opacity: 1,
+                rotate: [0, 5, -5, 0],
             }}
             transition={{
-                duration: 10,
+                delay: 0.5,
+                duration: 2,
                 ease: "easeInOut",
-                repeat: Infinity,
-                repeatType: "mirror"
+                y: {
+                    duration: 4,
+                    repeat: Infinity,
+                    repeatType: "mirror",
+                    ease: "easeInOut",
+                },
+                rotate: {
+                    duration: 10,
+                    repeat: Infinity,
+                    ease: "easeInOut",
+                }
             }}
         >
             <Lottie animationData={astronautAnimation} loop={true} />
